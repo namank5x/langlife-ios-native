9e4d5ed22c0665f4b31bd9f0317fea4c96c2a1ae
Naman <naman@wovenlabs.net>
2025-12-21 23:14:09 +0800

    Initial Commit

---
+++ langlife-ios-native/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  langlife-ios-native
+//
+//  Created by Naman Kamra on 2025/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ langlife-ios-native/langlife_ios_nativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  langlife_ios_nativeApp.swift
+//  langlife-ios-native
+//
+//  Created by Naman Kamra on 2025/12/21.
+//
+
+import SwiftUI
+
+@main
+struct langlife_ios_nativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
