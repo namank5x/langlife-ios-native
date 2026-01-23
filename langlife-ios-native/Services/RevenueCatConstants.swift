3bef4dd602e470da801a123eae96e6bf8daf782d
Naman <namank5x@gmail.com>
2026-01-23 13:20:57 +0800

    fix constants

--- langlife-ios-native/Services/RevenueCatConstants.swift
+++ langlife-ios-native/Services/RevenueCatConstants.swift
@@ -1,14 +1,4 @@
 enum RevenueCatConstants {
     static let entitlementID = "Lang Life Pro"
     static let offeringID = "default"
-
-    enum ProductID {
-        static let monthly = "monthly"
-        static let weekly = "subscription_weekly"
-    }
-
-    enum PackageID {
-        static let monthly = "monthly"
-        static let weekly = "weekly"
-    }
 }
